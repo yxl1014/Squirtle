a8b55dc4993d30f13fd44a3bcf7d4e7b45a4abb3
yxl1014 <1627419257@qq.com>
2022-06-07 16:35:20 +0800

    first commit

---
+++ src/yxl/demo/Test.java
@@ -0,0 +1,292 @@
+package yxl.demo;
+
+import java.util.Random;
+
+public class Test {
+
+    private final Random random = new Random();
+
+    private final int[] a = new int[100];
+    private final int[] magic = new int[100];
+    private final int[] b = new int[100];
+    private final int[] c = new int[100];
+
+    public Test() {
+        initMapA();
+        initMapB();
+        initMapC();
+        initMagic();
+    }
+
+    private void initMagic() {
+        int zero = 0;
+        while (zero < 25) {
+            int index = random.nextInt(1000) / 10;
+            if (magic[index] != 21)
+                magic[index] = 21;
+            else
+                continue;
+            zero++;
+        }
+
+        zero = 0;
+        while (zero < 25) {
+            int index = random.nextInt(1000) / 10;
+            if (magic[index] != 21 && magic[index] != 22)
+                magic[index] = 22;
+            else
+                continue;
+            zero++;
+        }
+
+        zero = 0;
+        while (zero < 25) {
+            int index = random.nextInt(1000) / 10;
+            if (magic[index] != 21 && magic[index] != 22 && magic[index] != 23)
+                magic[index] = 23;
+            else
+                continue;
+            zero++;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            if (magic[i] != 21 && magic[i] != 22 && magic[i] != 23)
+                magic[i] = 24;
+        }
+    }
+
+    private void initMapA() {
+        int zero = 0;
+        while (zero < 30) {
+            int index = random.nextInt(1000) / 10;
+            if (a[index] != 10)
+                a[index] = 10;
+            else
+                continue;
+            zero++;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            if (a[i] != 10)
+                a[i] = 11;
+        }
+    }
+
+    private void initMapB() {
+        int zero = 0;
+        while (zero < 30) {
+            int index = random.nextInt(1000) / 10;
+            if (b[index] != 10)
+                b[index] = 10;
+            else
+                continue;
+            zero++;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            if (b[i] != 10)
+                b[i] = 11;
+        }
+    }
+
+    private void initMapC() {
+        int zero = 0;
+        while (zero < 40) {
+            int index = random.nextInt(1000) / 10;
+            if (c[index] != 10)
+                c[index] = 10;
+            else
+                continue;
+            zero++;
+        }
+
+        for (int i = 0; i < 100; i++) {
+            if (c[i] != 10)
+                c[i] = 11;
+        }
+    }
+
+    public int fight1() {
+        int index = random.nextInt(100);
+        int type = magic(index);
+        if (type == 1)
+            return 0;
+
+        index = index + random.nextInt(1000);
+        switch (magic[index % 100]) {
+            case 21:
+                return 1;
+            case 22:
+                return 2;
+            case 23:
+                return 3;
+            case 24:
+                return 4;
+        }
+        return -1;
+    }
+
+    public int fight2() {
+        int index = random.nextInt(100);
+        int type = magic(index);
+        if (type == 1) {
+            index = random.nextInt(1000);
+            if (b[index % 100] == 10)
+                return -1;
+            else
+                return 0;
+        }
+
+        index = index + random.nextInt(1000);
+        switch (magic[index % 100]) {
+            case 21:
+                return 1;
+            case 22:
+                return 2;
+            case 23:
+                return 3;
+            case 24:
+                return 4;
+        }
+        return -404;
+    }
+
+    public int fight3() {
+        int index = random.nextInt(100);
+        int type = magic(index);
+        if (type == 1) {
+            index = random.nextInt(1000);
+            if (b[index % 100] == 10)
+                return -1;
+            else {
+                index = random.nextInt(1000);
+                if (c[index % 100] == 10)
+                    return -2;
+                else
+                    return 0;
+            }
+        }
+
+        index = index + random.nextInt(1000);
+        switch (magic[index % 100]) {
+            case 21:
+                return 1;
+            case 22:
+                return 2;
+            case 23:
+                return 3;
+            case 24:
+                return 4;
+        }
+        return -1;
+    }
+
+    public int magic(int index) {
+        if (a[index] == 10) return 0;
+        else return 1;
+    }
+
+    public void display() {
+        int rn = 0;
+        for (int i = 0; i < 100; i++) {
+            System.out.print(a[i] + " ");
+            if (++rn % 10 == 0)
+                System.out.println();
+        }
+    }
+
+    public void displayMagic() {
+        System.out.println("==========magic==========");
+        int rn = 0;
+        for (int i = 0; i < 100; i++) {
+            System.out.print(magic[i] + " ");
+            if (++rn % 10 == 0)
+                System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        int var0 = 0, var1 = 0, var2 = 0, var3 = 0, var4 = 0;
+        for (int i = 0; i < 10000; i++) {
+            switch (test.fight1()) {
+                case 0:
+                    var0++;
+                    break;
+                case 1:
+                    var1++;
+                    break;
+                case 2:
+                    var2++;
+                    break;
+                case 3:
+                    var3++;
+                    break;
+                case 4:
+                    var4++;
+            }
+        }
+        System.out.println("-----------fight1------------");
+        System.out.println("共攻击：" + (var0 + var1 + var2 + var3 + var4) + "次，" + "物理攻击：" + var0 + "次，" + "魔法攻击：" + (var1 + var2 + var3 + var4) + "次。");
+        System.out.println("毒：" + var1 + "次，" + "火：" + var2 + "次，" + "冰：" + var3 + "次，" + "雷：" + var4 + "次。");
+
+        int t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0;
+        for (int i = 0; i < 10000; i++) {
+            switch (test.fight2()) {
+                case -1:
+                    t2++;
+                    break;
+                case 0:
+                    t1++;
+                    break;
+                case 1:
+                    t3++;
+                    break;
+                case 2:
+                    t4++;
+                    break;
+                case 3:
+                    t5++;
+                    break;
+                case 4:
+                    t6++;
+            }
+        }
+        System.out.println("-----------fight3------------");
+        System.out.println("共攻击：" + (t1 + t2 + t3 + t4 + t5 + t6) + "次，" + "物理攻击：" + (t1 + t2) + "次，" + "魔法攻击：" + (t3 + t4 + t5 + t6) + "次。");
+        System.out.println("攻击：" + t1 + "次，" + "连击：" + t2 + "次。");
+        System.out.println("毒：" + t3 + "次，" + "火：" + t4 + "次，" + "冰：" + t5 + "次，" + "雷：" + t6 + "次。");
+
+
+        int q = 0, w = 0, e = 0, r = 0, t = 0, y = 0, u = 0;
+        for (int i = 0; i < 10000; i++) {
+            switch (test.fight3()) {
+                case -2:
+                    q++;
+                    break;
+                case -1:
+                    w++;
+                    break;
+                case 0:
+                    e++;
+                    break;
+                case 1:
+                    r++;
+                    break;
+                case 2:
+                    t++;
+                    break;
+                case 3:
+                    y++;
+                    break;
+                case 4:
+                    u++;
+            }
+        }
+        System.out.println("-----------fight3------------");
+        System.out.println("共攻击：" + (q + w + e + r + t + y + u) + "次，" + "物理攻击：" + (q + w + e) + "次，" + "魔法攻击：" + (r + t + y + u) + "次。");
+        System.out.println("攻击：" + (e+q) + "次，" + "连击：" + w + "次。");
+        System.out.println("普攻：" + e + "次，" + "分裂：" + q + "次。");
+        System.out.println("毒：" + r + "次，" + "火：" + t + "次，" + "冰：" + y + "次，" + "雷：" + u + "次。");
+    }
+}
---
+++ src/yxl/demo1/FindWay2.java
@@ -0,0 +1,178 @@
+package yxl.demo1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class FindWay2 {
+
+    private int[][] map;
+    private int h;
+    private int w;
+    private int[][] lastPath;
+    private int[][] result;
+    private int[] start;
+    private int[] end;
+
+    public FindWay2(int[][] map) {
+        this.map = map;
+        this.h = this.map.length;
+        this.w = this.map[0].length;
+
+        //初始化最短距离为数组全部元素的数量
+        this.result = new int[h][w];
+        this.lastPath = new int[h][w];
+        this.lastPath[0][0] = h * w;
+        this.start = Local(5);
+        this.end = Local(8);
+    }
+
+    //复制当前的最短路径
+    private void refreshPath(int[][] map, int[][] lastPath, int nowDepth) {
+        //先清空原有路径
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                lastPath[i][j] = 0;
+            }
+        }
+
+        //记录新的最短距离
+        lastPath[0][0] = nowDepth;
+
+        //再复制新的最短路径
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == 2) {
+                    lastPath[i][j] = map[i][j];
+                }
+            }
+        }
+    }
+
+    //判断map[i][j]是否在数组内，防止数组访问越界
+    public boolean inArr(int[][] map, int i, int j) {
+        return (i >= 0 && i < map.length && j >= 0 && j < map[i].length); //map[i]用到了逻辑与的短路效应，只有i未越界，后面的判断才会继续
+    }
+
+    //寻找从当前位置map[i][j]到达终点的最短路径和最短距离
+    //nowDepth表示递归深度（路的长度）
+    public void findMinWay(int[][] map, int i, int j, int[][] lastPath, int nowDepth) {
+
+        //比较nowDepth和lastPath[0][0]的大小，如果目前走的距离已经比最短路径长，则直接返回。剪枝，剪掉了当前路径这种选择
+        //但是，不将当前位置置为2，而是不做处理
+        if (nowDepth >= lastPath[0][0]) {
+            return;
+        } else {//继续尝试
+            if (/*map[end[0]][end[1]] == 8 && */i == end[0] && j == end[1]) {//已经找到出口，并且这条路比之前的最短路径都短，则记录路径，并更新最短距离
+                lastPath[0][0] = nowDepth;
+                map[end[0]][end[1]] = 2;
+                refreshPath(map, lastPath, nowDepth);
+                map[end[0]][end[1]] = 0;//先令终点为2，记录路径。然后置0，表示未走可走，这里也是回退一步的意思。
+                return;
+            } else {//还在找路
+                if (map[i][j] == 0 || map[i][j] == 5) {//当前路不是障碍物，可以尝试走
+                    //先假定当前位置可以走通
+                    map[i][j] = 2;
+                    //再寻找下一个位置，递归到目标
+                    if (inArr(map, i + 1, j))//进行边界判断
+                        findMinWay(map, i + 1, j, lastPath, nowDepth + 1);
+                    if (inArr(map, i, j + 1))
+                        findMinWay(map, i, j + 1, lastPath, nowDepth + 1);
+                    if (inArr(map, i - 1, j))
+                        findMinWay(map, i - 1, j, lastPath, nowDepth + 1);
+                    if (inArr(map, i, j - 1))
+                        findMinWay(map, i, j - 1, lastPath, nowDepth + 1);
+                    map[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    //得到str的坐标
+    private int[] Local(int s) {
+        int[] local = new int[2];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (map[i][j] == s) {
+                    local[0] = i;
+                    local[1] = j;
+                    return local;
+                }
+            }
+        }
+        return local;
+    }
+
+    public void start() {
+        findMinWay(map, start[0], start[1], lastPath, 0);
+        lastPath[start[0]][start[1]] = 5;
+        lastPath[end[0]][end[1]] = 8;
+    }
+
+    public void printMaze() {
+        System.out.println("打印迷宫如下============");
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printMinPath() {
+        System.out.println("最短路径如下============");
+        for (int i = 0; i < lastPath.length; i++) {
+            for (int j = 0; j < lastPath[i].length; j++) {
+                System.out.print(lastPath[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("最短距离为" + lastPath[0][0]);
+    }
+
+    public void printResult() {
+        for (int i = 0; i < lastPath.length; i++) {
+            for (int j = 0; j < lastPath[i].length; j++) {
+                if (map[i][j] != 0)
+                    result[i][j] = map[i][j];
+                if (lastPath[i][j] != 0)
+                    result[i][j] = lastPath[i][j];
+            }
+        }
+        System.out.println("-------result-------");
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        FindWay2 t1 = new FindWay2(new int[][]
+                {{5, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0},
+                        {0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1},
+                        {1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1},
+                        {1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 8},
+                        {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0},
+                        {0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+                        {0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0},
+                        {0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}});
+
+        //打印走前迷宫
+        t1.printMaze();
+
+        t1.start();
+
+        //打印最短路径
+        t1.printMinPath();
+
+        t1.printResult();
+    }
+}
+
+
+
+
